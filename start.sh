964eebbaf8d086518aa185e8635bacc0f2acc582
Justin Mullis <justin.mullis@gmail.com>
2019-08-03 16:26:54 -0400

    .

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ python3 src/display.py
 
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-./wifi-connect --clear=false
+./wifi-connect
 
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
