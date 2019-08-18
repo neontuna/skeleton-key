ea074a5c708ed68af41ea5be7262c16f37d34e85
Justin Mullis <justin.mullis@gmail.com>
2019-08-18 15:12:52 -0400

    rework start.sh script to have status LED before and after wifi-connect run

--- wifi-connect/start.sh
+++ wifi-connect/start.sh
@@ -3,6 +3,8 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 while [[ true ]]; do
+  python3 src/main.py & # start initial python instance but return and continue
+  
   # give pi a few seconds to finish boot and connect to wifi
   sleep 10
 
@@ -15,6 +17,8 @@ while [[ true ]]; do
       printf 'Starting WiFi Connect\n'
       ./wifi-connect -a 600
   fi
+  
+  pkill -9 -f main.py # kill old python instance and run new, but do not return control
   
   python3 src/main.py
 done
