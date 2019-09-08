edca14c38a4f64b11fda4facd61469d95f05e43c
Justin Mullis <justin.mullis@gmail.com>
2019-09-08 17:09:09 -0400

    .

--- battery/src/docker_host_ping.py
+++ battery/src/docker_host_ping.py
@@ -6,7 +6,33 @@ import re
 def packet_loss(interface):
     try:
         cmd = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q -p 22222 $DOCKER_HOST_IP 'ping -w 5 -I {0} 8.8.8.8'".format(interface)
-        ping_results = subprocess.run(cmd, shell=True, stdout=PIPE)
-        return re.search("\d+(?=% packet loss)", ping_results.stdout.strip().decode()) 
+        ping_results = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        return re.search("\d+(?=% packet loss)", ping_results.stdout.strip().decode())
     except subprocess.CalledProcessError:
         pass
+        
+i = 0
+
+while True;
+
+    eth0_packet_loss = packet_loss('eth0')
+    wlan0_packet_loss = packet_loss('wlan0')
+    wwan0_packet_loss = packet_loss('wwan0')
+    
+    if eth0_packet_loss == None | int(eth0_packet_loss.group()) > 50
+        print('eth0 offline!')
+    else
+        print('eth0 online!')
+    
+    if wlan0_packet_loss == None | int(wlan0_packet_loss.group()) > 50
+        print('wlan0 offline!')
+    else
+        print('wlan0 online!')
+    
+    if wwan0_packet_loss == None | int(wwan0_packet_loss.group()) > 50
+        print('wwan0 offline!')
+    else
+        print('wwan0 online!')
+        
+    i = i + 1
+    sleep(1)
