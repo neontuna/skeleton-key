158688c9208eb2934f9249639a10d5e63ad79491
Justin Mullis <justin.mullis@gmail.com>
2019-09-08 13:08:50 -0400

    .

--- battery/start.sh
+++ battery/start.sh
@@ -3,6 +3,14 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 while [[ true ]]; do
+  dbus-send --system --print-reply \
+            --dest=org.freedesktop.NetworkManager \
+            /org/freedesktop/NetworkManager \
+            org.freedesktop.DBus.Properties.Get \
+            string:"org.freedesktop.NetworkManager" \
+            string:"ActiveConnections"
+  sleep 10
+  
   python3 src/main.py
   sleep infinity
 done
