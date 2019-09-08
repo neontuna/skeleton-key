03d977d46e592e2ad158a88b4040d3b0c7fed98d
Justin Mullis <justin.mullis@gmail.com>
2019-09-08 17:50:25 -0400

    .

--- battery/src/docker_host_ping.py
+++ battery/src/docker_host_ping.py
@@ -2,6 +2,8 @@ from time import sleep
 from subprocess import PIPE
 import subprocess
 import re
+import NetworkManager
+import sys
         
 def packet_loss(interface):
     try:
@@ -30,9 +32,80 @@ def main():
         
         print("eth0: {0}, wlan0: {1}, wwan0: {2}".format(eth0_online, wlan0_online, wwan0_online))
         print("...")
+        
+        if eth0_online == False and wlan0_online == False and wwan0_online == False:
+            print("Failing over to cellular backup")
+            activate_connection('cellular')
+        elif wwan0_online == True and eth0_online == True or wlan0_online == True
+            print("Main connection online, disabling cellular backup")
+            deactivate_connection('cellular')
             
         i += 1
-        sleep(1)
+        sleep(30)
+        
+def activate_connection(name):
+    connections = NetworkManager.Settings.ListConnections()
+    connections = dict([(x.GetSettings()['connection']['id'], x) for x in connections])
+    conn = connections[name]
+
+    # Find a suitable device
+    ctype = conn.GetSettings()['connection']['type']
+    if ctype == 'vpn':
+        for dev in NetworkManager.NetworkManager.GetDevices():
+            if dev.State == NetworkManager.NM_DEVICE_STATE_ACTIVATED and dev.Managed:
+                break
+        else:
+            print("No active, managed device found")
+            sys.exit(1)
+    else:
+        dtype = {
+            '802-11-wireless': NetworkManager.NM_DEVICE_TYPE_WIFI,
+            '802-3-ethernet': NetworkManager.NM_DEVICE_TYPE_ETHERNET,
+            'gsm': NetworkManager.NM_DEVICE_TYPE_MODEM,
+        }.get(ctype,ctype)
+        devices = NetworkManager.NetworkManager.GetDevices()
+
+        for dev in devices:
+            if dev.DeviceType == dtype and dev.State == NetworkManager.NM_DEVICE_STATE_DISCONNECTED:
+                break
+        else:
+            print("No suitable and available %s device found" % ctype)
+            sys.exit(1)
+
+    # And connect
+    NetworkManager.NetworkManager.ActivateConnection(conn, dev, "/")
+    
+def deactivate_connection(name):
+    connections = NetworkManager.Settings.ListConnections()
+    connections = dict([(x.GetSettings()['connection']['id'], x) for x in connections])
+    conn = connections[name]
+
+    # Find a suitable device
+    ctype = conn.GetSettings()['connection']['type']
+    if ctype == 'vpn':
+        for dev in NetworkManager.NetworkManager.GetDevices():
+            if dev.State == NetworkManager.NM_DEVICE_STATE_ACTIVATED and dev.Managed:
+                break
+        else:
+            print("No active, managed device found")
+            sys.exit(1)
+    else:
+        dtype = {
+            '802-11-wireless': NetworkManager.NM_DEVICE_TYPE_WIFI,
+            '802-3-ethernet': NetworkManager.NM_DEVICE_TYPE_ETHERNET,
+            'gsm': NetworkManager.NM_DEVICE_TYPE_MODEM,
+        }.get(ctype,ctype)
+        devices = NetworkManager.NetworkManager.GetDevices()
+
+        for dev in devices:
+            if dev.DeviceType == dtype and dev.State == NetworkManager.NM_DEVICE_STATE_ACTIVATED:
+                break
+        else:
+            print("No suitable and available %s device found" % ctype)
+            sys.exit(1)
+
+    # And disconnect
+    NetworkManager.NetworkManager.DeactivateConnection(dev, "/")
     
 if __name__ == '__main__':
     main()
