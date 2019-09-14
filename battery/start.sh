692e2a64a38875c92ffbc7fce49c62a33d335634
Justin Mullis <justin.mullis@gmail.com>
2019-09-14 14:03:56 -0400

    .

--- battery/start.sh
+++ battery/start.sh
@@ -2,26 +2,25 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+# give pi a few seconds to finish boot and connect to wifi
+sleep 10
+
+# check for active wifi connection
+iwgetid -r
+
+if [ $? -eq 0 ]; then
+    printf 'Skipping WiFi Connect\n'
+else
+    printf 'Starting WiFi Connect\n'
+    ./wifi-connect -a 600
+fi
+
+# get wlan1 ready for monitor mode
+airmon-ng start wlan1
+
 while [[ true ]]; do
   python3 src/main.py & # start initial python instance but return and continue
   PID1=$!
-  
-  # give pi a few seconds to finish boot and connect to wifi
-  sleep 10
-  
-  # check for active wifi connection
-  iwgetid -r
-  
-  if [ $? -eq 0 ]; then
-      printf 'Skipping WiFi Connect\n'
-  else
-      printf 'Starting WiFi Connect\n'
-      ./wifi-connect -a 600
-  fi
-  
-  # get wlan1 ready for monitor mode
-  airmon-ng check kill
-  airmon-ng start wlan1
   
   wait $PID1
   echo 'Monitor script stopped, restarting . . .'
