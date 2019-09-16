421fac2348c41d35f429d57a8b3cf0d8bafcb5c6
Justin Mullis <justin.mullis@gmail.com>
2019-09-16 16:25:58 -0400

    speedtest

---
+++ watchman/src/speedmon.py
@@ -0,0 +1,15 @@
+import speedtest
+
+def get_speedtest_results():
+    s = speedtest.Speedtest()
+    s.get_servers(servers)
+    s.get_best_server()
+    s.download(threads=None)
+    s.results.share()
+    print(s.results.dict())
+    pass
+
+def main():
+    thread = threading.Thread(target=get_speedtest_results)
+	thread.start()
+    pass
