a4a8b56096e2a5d4ed01ae6397ab81e1c90b7ac8
Justin Mullis <justin.mullis@gmail.com>
2019-09-16 16:43:15 -0400

    speedmon updates

--- watchman/src/speedmon.py
+++ watchman/src/speedmon.py
@@ -1,12 +1,20 @@
 import speedtest
+import sys, getopt, os
+from balena import Balena
+
+balena = Balena()
+balena.auth.login_with_token(os.environ['BALENA_API_KEY'])
+
+def update_tag(tag, variable):
+    balena.models.tag.device.set(os.environ['BALENA_DEVICE_UUID'], str(tag), str(variable))
 
 def get_speedtest_results():
     s = speedtest.Speedtest()
-    s.get_servers(servers)
     s.get_best_server()
     s.download(threads=None)
-    s.results.share()
     print(s.results.dict())
+    update_tag('download_speed', s.results.download_speed)
+    update_tag('ping', s.results.ping)
     pass
 
 def main():
