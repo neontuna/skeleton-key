fd7fb94abc926d23a14c8e05e7db6e0d6d558e76
Justin Mullis <justin.mullis@gmail.com>
2019-09-16 20:22:47 -0400

    .

--- watchman/start.sh
+++ watchman/start.sh
@@ -3,17 +3,17 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # give pi a few seconds to finish boot and connect to wifi
-sleep 10
-
-# check for active wifi connection
-iwgetid -r
-
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    ./wifi-connect -a 600
-fi
+# sleep 10
+# 
+# # check for active wifi connection
+# iwgetid -r
+# 
+# if [ $? -eq 0 ]; then
+#     printf 'Skipping WiFi Connect\n'
+# else
+#     printf 'Starting WiFi Connect\n'
+#     ./wifi-connect -a 600
+# fi
 
 sleep infinity
 
