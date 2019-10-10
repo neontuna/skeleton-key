825e84663c2b4b071a1c1fc185088c49b462b6bd
Justin Mullis <justin.mullis@gmail.com>
2019-10-10 09:58:08 -0400

    re-enabling

--- watchman/start.sh
+++ watchman/start.sh
@@ -3,7 +3,7 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # give pi a few seconds to finish boot and connect to wifi
-# printf 'Sleeping on boot, UDEV race fix\n'
+printf 'Sleeping on boot, UDEV race fix\n'
 sleep 20
 
 # check for active wifi connection
@@ -13,12 +13,11 @@ if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    ./wifi-connect
-    # ./wifi-connect -a 600 -i wint
+    ./wifi-connect -a 600 -i wint
 fi
 
 # get wlan1 ready for monitor mode
-# airmon-ng start wext
+airmon-ng start wext
 
 # while [[ true ]]; do
 #   python3 src/main.py & # start initial python instance but return and continue
